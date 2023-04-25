09666b28aff52b049381e15d27f3fbf92ce25cd6
Saulo Klein Nery <saulokn@outlook.com>
2023-04-24 22:58:04 -0300

    feat: Adiciona método run e criação de threads

--- src/Game.java
+++ src/Game.java
@@ -6,26 +6,84 @@ public class Game extends Canvas implements Runnable{
     public static int WIDTH = 240;
     public static int HEIGHT = 120;
     public static int SCALE = 3;
+    public static boolean isRunning;
+    private Thread thread;
 
     public Game() {
         this.setPreferredSize(new Dimension(WIDTH*SCALE, HEIGHT*SCALE));
+        this.initFrame();
     }
 
     public static void main(String[] args) {
         Game game = new Game();
+
+        game.start();
+    }
+
+    public synchronized void start() {
+        this.thread = new Thread(this);
+        isRunning = true;
+        this.thread.start();
+    }
+
+    public synchronized void stop() {
+        isRunning = false;
+        try {
+            this.thread.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void initFrame() {
         JFrame frame = new JFrame("Pong Game");
 
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.add(game);
+        frame.add(this);
         frame.pack();
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
     }
 
+    public void tick() {
+
+    }
+
+    public void render() {
+
+    }
+
     @Override
     public void run() {
+        long lastTime = System.nanoTime();
+        double amountOfTicks = 60;
+        double ns = Math.pow(10, 9) / amountOfTicks;
+        double delta = 0;
+        int frames = 0;
+        double timer = System.currentTimeMillis();
 
+        while(isRunning) {
+            long now = System.nanoTime();
+            delta += (now - lastTime) / ns;
+            lastTime = now;
+
+            if(delta >= 1) {
+                this.tick();
+                this.render();
+                frames++;
+                delta--;
+            }
+
+            if(System.currentTimeMillis() - timer >= 1000){
+                System.out.println("FPS: " + frames);
+                frames = 0;
+                timer += 1000;
+            }
+
+        }
+
+        this.stop();
     }
 
 }
---
+++ src/Player.java
@@ -0,0 +1,57 @@
+import java.awt.*;
+
+public class Player {
+    private int x;
+    private int y;
+    private int width;
+    private int height;
+
+    public Player(int x, int y, int width, int height) {
+        this.setHeight(height);
+        this.setWidth(width);
+        this.setX(x);
+        this.setY(y);
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getHeight() {
+        return this.height;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getWidth() {
+        return this.width;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getX() {
+        return this.x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getY() {
+        return this.y;
+    }
+
+    public void tick() {
+
+    }
+
+    public void render(Graphics graphics) {
+        graphics.setColor(Color.blue );
+        graphics.fillRect(this.getX(),this.getY(),this.getWidth(),this.getHeight());
+    }
+
+}
